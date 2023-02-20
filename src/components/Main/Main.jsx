406de13b2220c88cf838c78abfe03b91630441ac
zairov01 <106994015+AmirbekCode@users.noreply.github.com>
2023-02-20 14:58:42 +0500

    style edit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,7 +5,7 @@ export default  function Main() {
     return (
         <div className="Main">
            <div className="Main_section">
-                <h2>Amirbek</h2>
+                
             </div> 
         </div>
     )
